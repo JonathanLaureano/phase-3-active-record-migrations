3c2cefdf976e78ac7b22b59ccc3a0b8bcdfb05fd
JonathanLaureano <JonathanLaureano20@gmail.com>
2022-02-27 21:11:58 -0500

    commit tree bf59b16393092e104b858876f0507146cdd6bf5b

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,14 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+end
+
+a = Artist.new(name: 'Jon')
+# => #<Artist id: nil, name: "Jon", genre: nil, age: nil, hometown: nil>
+
+a.age = 30
+# => 30
+
+a.save
+# => true
+
+Artist.create(name: 'Kelly')
